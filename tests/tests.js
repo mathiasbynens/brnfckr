1ba11256d449ea9e24bed308946af4c36ee02889
Mathias Bynens <mathias@qiwi.be>
2013-03-24 22:03:30 +0100

    Add brainfuck minifier in brainfuck to tests

--- tests/tests.js
+++ tests/tests.js
@@ -39,6 +39,12 @@
 				'description': 'Hello world',
 				'original': '+++++ +++++             initialize counter (cell #0) to 10\n[                       use loop to set the next four cells to 70/100/30/10\n    > +++++ ++              add  7 to cell #1\n    > +++++ +++++           add 10 to cell #2 \n    > +++                   add  3 to cell #3\n    > +                     add  1 to cell #4\n    <<<< -                  decrement counter (cell #0)\n]                   \n> ++ .                  print \'H\'\n> + .                   print \'e\'\n+++++ ++ .              print \'l\'\n.                       print \'l\'\n+++ .                   print \'o\'\n> ++ .                  print \' \'\n<< +++++ +++++ +++++ .  print \'W\'\n> .                     print \'o\'\n+++ .                   print \'r\'\n----- - .               print \'l\'\n----- --- .             print \'d\'\n> + .                   print \'!\'\n> .                     print \'\\n\'',
 				'minified': '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.'
+			},
+			{ // http://codegolf.stackexchange.com/a/4815/4035
+				'description': 'brainfuck minifier in brainfuck',
+				'original': '>>>,\n[\n[->+>+<<]             copy input\n\n<<<+++++++++++++      fill cell with 13 for subtraction\n[->+>+>+<<<]          and duplicate some times\n\n>>>[->---<]           sub 39\n>----                 43\n\n[                     plus\n-\n[                     comma\n-\n[                     minus\n-\n[                     dot\n\n<<[->>-<<]>>-         60\n\n[                     less than\n--\n[                     greater than\n\n<<<[->>>--<<<]>>>     86\n---                   91\n\n[                     open square bracket\n--\n[                     closing square bracket\n\n>[-]                  remove copy\n\n]]]]]]]]\n\n>[.[-]>]              the copy has not been removed; print\n\n<<[-]<<<[-]>[-]>      clean up\n\n,]',
+				'minified': '>>>,[[->+>+<<]<<<+++++++++++++[->+>+>+<<<]>>>[->---<]>----[-[-[-[<<[->>-<<]>>-[--[<<<[->>>--<<<]>>>---[--[>[-]]]]]]]]]>[.[-]>]<<[-]<<<[-]>[-]>,]'
+
 			}
 		],
 
